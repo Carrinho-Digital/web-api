74723f39af8d1b08de4386f73180927c790eae51
eclesio <eclesiomelo.1@gmail.com>
2020-04-12 15:05:50 -0400

    markets can now add only one address

--- src/domain/users/useCases/upsertUserAddress.js
+++ src/domain/users/useCases/upsertUserAddress.js
@@ -2,10 +2,16 @@ const User = require('../models/user');
 
 const {
   NotFound: NotFoundException,
+  General: GeneralException,
 } = require('../../../exceptions');
 
 function buildUpsertUserAddress() {
-  return async function upsertUserAddress(addressId, addressValues, userId) {
+  return async function upsertUserAddress(
+    addressId,
+    addressValues,
+    userId,
+    isMarket,
+  ) {
     if (addressId) {
       const updatedValue = await User.findOneAndUpdate(
         { '_id': userId, 'addresses._id': addressId },
@@ -24,6 +30,10 @@ function buildUpsertUserAddress() {
 
       if (!user) {
         throw new NotFoundException('User could not be found', 404);
+      }
+
+      if (isMarket && user.addresses.length > 0) {
+        throw new GeneralException('Markets cannot add more then 1 address');
       }
 
       user.addresses.push(addressValues);
