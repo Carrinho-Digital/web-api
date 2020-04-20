2ed01643699ce05303bd48018d5c0f63415e94a0
eclesio <eclesiomelo.1@gmail.com>
2020-04-20 18:15:58 -0400

    segregate market login from customer

--- src/domain/users/useCases/login.js
+++ src/domain/users/useCases/login.js
@@ -1,19 +1,30 @@
 const User = require('../models/user');
 
 function buildLogin({ encrypt, jwt, logger }) {
-  return async function login(email, password) {
+  return async function login(
+    email, password, loginType,
+  ) {
     try {
       const user = await User.findOne({ email });
 
-      if (!user) return null;
-      if (user.isDeleted) return null;
+      if (!user) {
+        return null;
+      }
+
+      if (user.isDeleted) {
+        return null;
+      }
+
+      if (user.type !== loginType) {
+        return null;
+      }
 
       const passwordMatch = await encrypt
         .compare(password, user.password);
 
       if (!passwordMatch) return null;
 
-      return await jwt.sign({ email: user.email });
+      return await jwt.sign({ email: user.email, type: user.type });
     } catch (exception) {
       logger.error('login exception', {
         message: exception.message,
